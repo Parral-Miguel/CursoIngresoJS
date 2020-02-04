26e15aacf1a28d7b34fd2d9756980f8f56ea4298
alumno <alumno@utnfra.com.ar>
2020-02-03 22:18:00 -0300

    ejercicios 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,13 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    var leyenda;
+    var resultado;
+
+    leyenda = "ingrese un dato";
+
+    resultado = prompt(leyenda);
+    
+    alert(resultado);
 }
 
